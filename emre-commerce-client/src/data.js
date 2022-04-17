b6970e71e6db1ea31cd92dde677034c253f593e0
Emre KAYACIK <emrekayacik@eskisehir.edu.tr>
2022-04-17 20:07:19 +0300

    categories and products added

--- emre-commerce-client/src/data.js
+++ emre-commerce-client/src/data.js
@@ -7,7 +7,7 @@ export const sliderItems = [
     },
     {
         id:2,
-        img:"https://eskipaper.com/images/fashion-model-red-dress-1.jpg",
+        img:"https://www.pngarts.com/files/2/Model-Transparent-Background-PNG.png",
         title: "SUMMER SALE",
         description: "İster günlük hayatta, ister özel günlerde giyebileceğiniz kadın giyim modellerini gördünüz mü? Modaya en uygun kadın kıyafetleri için hemen tıklayın!"
     },
@@ -23,4 +23,68 @@ export const sliderItems = [
         title: "SUMMER SALE",
         description: "İster günlük hayatta, ister özel günlerde giyebileceğiniz kadın giyim modellerini gördünüz mü? Modaya en uygun kadın kıyafetleri için hemen tıklayın!"
     },
+];
+
+export const categories = [
+    {
+        id:1,
+        img:"https://images.pexels.com/photos/7533345/pexels-photo-7533345.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        title: "YOUR DESIGN!",
+        
+    },
+    {
+        id:2,
+        img:"https://images.pexels.com/photos/10660920/pexels-photo-10660920.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        title: "PANTS",
+        
+    },
+    {
+        id:3,
+        img:"https://images.pexels.com/photos/10661059/pexels-photo-10661059.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        title: "SUMMERWEAR",
+        
+    }
+];
+
+export const popularProducts = [
+    {
+        id:1,
+        img:"https://www.vintageindustries.nl/download_front/yxfbur1758/_overview_large_/2217_Arrow_Jacket_Dark_tan.png"
+        
+    },
+    {
+        id:2,
+        img:"https://www.vintageindustries.nl/download_front/ogufmv1546/_overview_large_/2101_Welder_jacket_Night_sky.png"
+        
+    },
+    {
+        id:3,
+        img:"https://www.vintageindustries.nl/download_front/syinqx1484/_overview_large_/2109_Grafton_anorak_Black_2_.png"
+        
+    },
+    {
+        id:4,
+        img:"https://www.vintageindustries.nl/download_front/qzgvet1531/_overview_large_/30101_Ashore_softshell_jacket_Black_2_.png"
+        
+    },
+    {
+        id:5,
+        img:"https://www.vintageindustries.nl/download_front/lapgbm1604/_overview_large_/30103_Levin_jacket_Woodland_camo.png"
+        
+    },
+    {
+        id:6,
+        img:"https://www.vintageindustries.nl/download_front/schkyb1297/_overview_large_/5012_Corduroy_cap_Beige.png"
+        
+    },
+    {
+        id:7,
+        img:"https://www.vintageindustries.nl/download_front/rhumgc1313/_overview_large_/2075_Leap_jacket_Navy_Midnight_OS.png"
+        
+    },
+    {
+        id:8,
+        img:"https://www.vintageindustries.nl/download_front/hrvueb1567/_overview_large_/3030_Class__Sherpa_Red_Check.png"
+        
+    }
 ];
--- emre-commerce-client/src/pages/Home.jsx
+++ emre-commerce-client/src/pages/Home.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Announcement from '../components/Announcement';
 import Slider from '../components/Slider';
-
+import Categories from '../components/Categories';
+import Products from '../components/Products';
 
 const Home = () => {
   return (
@@ -10,6 +11,8 @@ const Home = () => {
         <Announcement />
         <Navbar />
         <Slider />
+        <Categories />
+        <Products />
     </>
   );
 }
