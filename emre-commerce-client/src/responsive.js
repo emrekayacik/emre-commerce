a9d56c383caa0f853be6039ec29be16053b81447
Emre KAYACIK <emrekayacik@eskisehir.edu.tr>
2022-08-27 23:47:59 +0300

    responsive width decresed

--- emre-commerce-client/src/responsive.js
+++ emre-commerce-client/src/responsive.js
@@ -2,7 +2,7 @@ import {css} from "styled-components"
 
 export const mobile = (props) => {
     return css`
-    @media only screen and (max-width:380px){
+    @media only screen and (max-width:768px){
         ${props}
     }
     `
